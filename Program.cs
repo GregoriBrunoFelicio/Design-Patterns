72bd12f14b2c04dfe89fc13330e6cd28770111ce
gregori bruno felicio <gregory.wow@hotmail.com>
2021-06-04 21:01:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Design_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pedido = new Pedido
+            {
+                Total = 10
+            };
+
+            var banco = new Itau();
+
+            var calculadoraDeJuros = new CalculadorDeJuros(banco);
+
+            Console.WriteLine(calculadoraDeJuros.CalcularJuros(pedido));
+        }
+    }
+}
---
+++ Strategy.cs
@@ -0,0 +1,50 @@
+﻿namespace Design_Patterns
+{
+
+    public class Pedido
+    {
+        public decimal Total { get; set; }
+    }
+
+
+    public class Bradesco : CalculoDeJuros
+    {
+        public decimal CalcularJuros(object obj)
+        {
+            var pedido = obj as Pedido;
+            return pedido.Total * 5;
+        }
+    }
+
+    public class Itau : CalculoDeJuros
+    {
+        public decimal CalcularJuros(object obj)
+        {
+            var pedido = obj as Pedido;
+            return pedido.Total * 10;
+        }
+    }
+
+    public interface CalculoDeJuros
+    {
+        decimal CalcularJuros(object obj);
+    }
+
+    public class CalculadorDeJuros
+    {
+        private readonly CalculoDeJuros calculoDeJuros;
+
+        public CalculadorDeJuros(CalculoDeJuros calculoDeJuros)
+        {
+            this.calculoDeJuros = calculoDeJuros;
+        }
+
+        public decimal CalcularJuros(object obj)
+        {
+            var pedido = obj as Pedido;
+            return this.calculoDeJuros.CalcularJuros(pedido);
+
+        }
+    }
+
+}
