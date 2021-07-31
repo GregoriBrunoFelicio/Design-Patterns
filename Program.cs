e1995707e787a8e02134d52144d71f0674275521
gregori bruno felicio <gregory.wow@hotmail.com>
2021-07-31 13:37:00 -0300

    strategy

--- Program.cs
+++ Program.cs
@@ -1,11 +1,59 @@
-﻿namespace Design_Patterns
+﻿using System;
+
+namespace Design_Patterns
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var singleton = Singleton.GetInstance();
-            var x = Singleton.GetInstance();
+            var calculo = new Calculo(new Cliente3());
+            calculo.Calcular(45);
+        }
+    }
+
+    interface ICalculo
+    {
+        void Calcular(int valor);
+    }
+
+    class Calculo : ICalculo
+    {
+        private readonly ICalculo calculo;
+
+        public Calculo(ICalculo calculo) => this.calculo = calculo;
+
+        public void Calcular(int valor)
+        {
+            calculo.Calcular(valor);
+        }
+    }
+
+    public abstract class Cliente
+    {
+        public string Nome { get; set; }
+    }
+
+    public class Cliente1 : ICalculo
+    {
+        public void Calcular(int valor)
+        {
+            Console.WriteLine(2 + valor);
+        }
+    }
+
+    public class Cliente2 : ICalculo
+    {
+        public void Calcular(int valor)
+        {
+            Console.WriteLine(3 + valor);
+        }
+    }
+
+    public class Cliente3 : ICalculo
+    {
+        public void Calcular(int valor)
+        {
+            Console.WriteLine(4 + valor);
         }
     }
 }
